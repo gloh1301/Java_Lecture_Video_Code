52687c50b9d9d4f5b0ad27542fd63d96cd2aa646
Grant <grant_d_lohman@aol.com>
2023-09-06 17:15:03 -0500

    added if statement code

--- src/main/java/week_1_if_else/FirstIfStatement.java
+++ src/main/java/week_1_if_else/FirstIfStatement.java
@@ -1,10 +1,21 @@
 package week_1_if_else;
 
+import static input.InputUtils.doubleInput;
 import static input.InputUtils.stringInput;
 
 public class FirstIfStatement {
     public static void main(String[] args) {
         String message = stringInput("Enter some text: ");
         System.out.println("Your message is " + message);
+
+        double price = doubleInput("What is the price of chips? ");
+
+        if (price < 0) {
+            System.out.println("Price can't be negative");
+        } else if (price == 0) {
+            System.out.println("Its free!");
+        } else {
+            System.out.println("Price is $" + price);
+        }
     }
 }
---
+++ src/main/java/week_1_if_else/Temp.java
@@ -0,0 +1,14 @@
+package week_1_if_else;
+
+public class Temp {
+    public static void main(String[] args) {
+        double tempYesterday = 100;
+        double tempToday = 65;
+
+        if (tempYesterday > tempToday) {
+            System.out.println("Yesterday was warmer");
+        } else {
+            System.out.println("Today is warmer");
+        }
+    }
+}
