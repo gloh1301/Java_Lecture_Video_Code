f60b4b5af7d592eaa11a5411d6734c1e505761bb
Grant <grant_d_lohman@aol.com>
2023-09-06 17:22:58 -0500

    added user input to temp

--- src/main/java/week_1_if_else/Temp.java
+++ src/main/java/week_1_if_else/Temp.java
@@ -1,9 +1,11 @@
 package week_1_if_else;
 
+import static input.InputUtils.doubleInput;
+
 public class Temp {
     public static void main(String[] args) {
         double tempYesterday = 100;
-        double tempToday = 65;
+        double tempToday = doubleInput("What is the temperature today? ");
 
         if (tempYesterday > tempToday) {
             System.out.println("Yesterday was warmer");
